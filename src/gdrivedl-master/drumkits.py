2966a005f76947e493a4ab7514b3bc9b6ca8a410
jesusyanez <96055925+jesusyanez@users.noreply.github.com>
2022-07-18 01:48:18 -0400

    completed

--- src/gdrivedl-master/drumkits.py
+++ src/gdrivedl-master/drumkits.py
@@ -1,6 +1,15 @@
+# This program downloads drumkits from reddit.com/r/drumkits
+# It is limited to google drive link
+# Requirements: gdown
+
 from get_time import right_now
 from urllib.request import urlopen
+import random
 import json
+import gdrivedl
+import os
+import gdown
+import re
 
 print(r"""
 ██████╗ ██████╗ ██╗   ██╗███╗   ███╗██╗  ██╗██╗████████╗███████╗      ██████╗ ██╗     
@@ -20,33 +29,86 @@ Created for r/drumkits by Aesap (https://twitter.com/_aesap)
 # print('Last Update: ' + str(right_now()))
 # print('Trending on https://reddit.com/r/drumkits at ' + right_now() +'\n\n')
 
-# Get reddit json data
+#Random request to avoid getting blocked by reddit
+# obfuscate_list = ['sports', 'funny', 'leagueoflegends', 'new', 'MadeMeSmile', 'canada', 'AskReddit', 'entertainment', 'space', 'technology', 'nodejs', 'gaming', 'unexpected', 'politics', 'PublicFreakout', 'movies', 'nba']
+# fake_url = 'https://reddit.com/r/' + random.choice(obfuscate_list) + '/.json'
+# print('pinged: ' +fake_url)
+# fake_ping = urlopen(fake_url)
+
+
+# Get r/Drumkits json data
 url = "https://www.reddit.com/r/Drumkits/.json"
 response = urlopen(url)
 data_json = json.loads(response.read())
 
+
+# json data --> Print and make data structures
 def find_drumkits(kit):
     return kit
 
 map_drumkits = map(find_drumkits, data_json['data']['children'])
-# print(map_drumkits)
 count = 1
 url_list = []
 title_list = []
+index_list = []
 for x in map_drumkits:
-    if x['data']['domain'] == 'drive.google.com':
+    if x['data']['domain'] == 'drive.google.com': #filter only gdrive links
         print("["+str(count)+"] "+x['data']['title'])
         title_list.append(x['data']['title'])
         url_list.append(x['data']['url'])
+        index_list.append(count)
         count += 1
 
-# print(url_list)
+# Create dictionaries for downloading with selected key
+title_dict = {key:value for (key, value) in zip(index_list, title_list)}
+url_dict = {key:value for (key, value) in zip(index_list, url_list)}
 
 # Print selection input
 to_download_str = input('Enter drumkits you want to download:\n')
 
 selected_list = to_download_str.split(", ")
 
+# download function
+print('Now downloading ' + str(len(selected_list)) + ' drumkits. Check back in a 5 minutes.')
+
 for x in selected_list:
-    print(x)
-print (selected_list)
+    if 'folder' in url_dict[int(x)]:
+        print('Downloading [' + x + ']: ' + title_dict[int(x)])
+        filtered_title = re.sub(r'[^\w]', ' ', title_dict[int(x)])
+        output = './Drumkits/' + filtered_title.replace(' ', '_')
+        # command = 'py gdrivedl.py ' + url + ' -P ' + title #windows
+        command = 'python3 gdrivedl.py ' + url_dict[int(x)] + ' -P ' + output #linux
+        os.system(command)
+
+    elif 'file' in url_dict[int(x)]:
+        print('Downloading [' + x + ']: ' + title_dict[int(x)])
+        output = './Drumkits/' + title_dict[int(x)] + '.zip'
+        gdown.download(url=url_dict[int(x)], output=output, quiet=False, fuzzy=True)
+
+    else: 
+        print('The url for [' + x + ']: '+ url_dict[int(x)] + ' is messed up.')
+
+
+
+
+
+                                                                      
+print(r"""
+██████   ██████  ██     ██ ███    ██ ██       ██████   █████  ██████  
+██   ██ ██    ██ ██     ██ ████   ██ ██      ██    ██ ██   ██ ██   ██ 
+██   ██ ██    ██ ██  █  ██ ██ ██  ██ ██      ██    ██ ███████ ██   ██ 
+██   ██ ██    ██ ██ ███ ██ ██  ██ ██ ██      ██    ██ ██   ██ ██   ██ 
+██████   ██████   ███ ███  ██   ████ ███████  ██████  ██   ██ ██████  
+
+
+ ██████  ██████  ███    ███ ██████  ██      ███████ ████████ ███████ 
+██      ██    ██ ████  ████ ██   ██ ██      ██         ██    ██      
+██      ██    ██ ██ ████ ██ ██████  ██      █████      ██    █████   
+██      ██    ██ ██  ██  ██ ██      ██      ██         ██    ██      
+ ██████  ██████  ██      ██ ██      ███████ ███████    ██    ███████ 
+                                                                     
+                                                                     
+You may close this window.
+""")                             
+
+
--- src/gdrivedl-master/test.py
+++ src/gdrivedl-master/test.py
@@ -24,26 +24,45 @@
 # [21] https://drive.google.com/file/d/1g32OrfqrzCgebrwSBn1CaJF_-m9YEUSv/view?usp=sharing
 # [22] https://drive.google.com/file/d/1nh8TE25SCY3fjZYZ7vQVYAnfRMzv9ZR_/view?usp=sharing
 
-import gdown
+# import gdown
 
 # same as the above, and you can copy-and-paste a URL from Google Drive with fuzzy=True
 # url = "https://drive.google.com/file/d/1HKfmfvm10RmaGUkJEvWJ6rgzr78ZbFOp/view?usp=sharing"
 # output = "drumkit.zip"
 # gdown.download(url=url, output=output, quiet=False, fuzzy=True)
 
-
-
+title_dict = {1: "Darkboy all drum Kits that i have(i think it's the coolest DarkBoy kits). I think ihelp somebody with these kits,use it,drop your beats and i believe in u. love u guys and send piece around u :^", 2: '200+ Synthesized Percussion Sounds I made', 3: 'Hello There - New Drum Kit', 4: 'PinkGrillz88 - Untitled Drum Kit 2022', 5: '[DAKe] Graveyard Fields Drum Kit', 6: 'PXS Global SuperTrap Essentials V2 !REUPLOADED!', 7: '@mymagicpotion - DONDA DRUM KIT ALL DRUM AND BASS SOUNDS FROM DONDA', 8: '@mymagicpotion - DONDA 2 DRUM KIT ALL DRUM AND BASS SOUNDS FROM DONDA 2', 9: 'Experiment Club Production Suite', 10: 'BENO/YALE BASS SYNTH SOUND (FOUND, 99.8% ACCURATE)', 11: 'HUNCHOJACK DRUMKIT', 12: 'A$APROCKY DRUMKIT', 13: 'TRAVIS SCOTT - RODEO DRUMKIT [EXACT SOUNDS]', 14: 'Rio Leyva - Alien Drum Kit !REUPLOADED!', 15: 'Rio Leyva - Alien Drum Kit !REUPLOADED!', 16: 'my stash kit', 17: 'UNDERGROUND (DIFF GENRES) ARTIST SAMPLE PACKS (LOOP &amp; DRUM KITS)', 18: '175+ One Shots &amp; Sounds From Various Songs', 19: 'Hoolade — Sick (Drum Kit) 2022', 20: 'JACKBOYS DRUMKIT [DRUMS/ONESHOTS]', 21: 'TRAVIS SCOTT-OWLPHARAOH DRUMKIT', 22: 'T-Rex 808 [Selfmade, certified speaker blower]', 23: '@emeeelya - Sqüare (drum kit).rar'}
+url_list = {1: 'https://drive.google.com/drive/folders/1G4C_zyfzUwSlOG-pstDwiqb0Qxjc3sUO?usp=sharing', 2: 'https://drive.google.com/drive/folders/1jHVUf15i18Sl_mMTNUdOQjH9Mb0qhIsP?usp=sharing', 3: 'https://drive.google.com/drive/folders/1VEWF7hIuvqFnKN35LqG4wRELkrX0u66p?usp=sharing', 4: 'https://drive.google.com/drive/folders/1Iutl5GPdBU0Sps1zqY1HdYSw_0VWCZo2?usp=sharing', 5: 'https://drive.google.com/drive/folders/1x0uGFhjYZqOYd-vEqwvVitRk5GCoAlzv?usp=sharing', 6: 'https://drive.google.com/drive/folders/1XVpJyWDIUHbmjbMHrU0OsNXeYy_RsupM?usp=sharing', 7: 'https://drive.google.com/drive/folders/14PjpIRtPwvIInqngZoVJ1HTdMcHJfQNh?usp=sharing', 8: 'https://drive.google.com/drive/folders/1VaXxeMyZbuN6Qf2L_tOkjdZVFt-uOPaZ?usp=sharing', 9: 'https://drive.google.com/drive/folders/176dauAKpzN8Uo5KpGY84zQcLn7KKRy53?usp=sharing', 10: 'https://drive.google.com/drive/folders/1MYJ_pQgaRX69Aw5ss1FqCni1Dw31a_S4?usp=sharing', 11: 'https://drive.google.com/file/d/1HKfmfvm10RmaGUkJEvWJ6rgzr78ZbFOp/view?usp=sharing', 12: 'https://drive.google.com/drive/u/1/my-drive', 13: 'https://drive.google.com/file/d/1ysrt8oN7AATFHLxG_iW2w3nqflXcaKr7/view?usp=sharing', 14: 'https://drive.google.com/drive/u/1/folders/1KJ51wdZfw_a5R3FTC1vIISKcsF_qZrJY', 15: 'https://drive.google.com/drive/folders/1KJ51wdZfw_a5R3FTC1vIISKcsF_qZrJY?usp=sharing', 16: 'https://drive.google.com/drive/folders/1YT-L8lR-FVs5AqcFUdXhAf5Htw7bVppx?usp=sharing', 17: 'https://drive.google.com/drive/folders/1moxaIWrnYxSk4It-wH2uyk-ohLBqg8Eb?usp=sharing', 18: 'https://drive.google.com/file/d/1Vn9uYvI49gp0XedJcNJBhIBInQbZsL5P/view?usp=sharing', 19: 'https://drive.google.com/file/d/1beQtNpmOM7PoijbEZSz92SZPEOZ2hvPr/view', 20: 'https://drive.google.com/file/d/1iBfCPFhymOzsmFQ9T-FoWkUHh7d0h0Qx/view?usp=sharing', 21: 'https://drive.google.com/file/d/1_VcFfjJanL5aIvUsGlUnmEyD7CD2xhJi/view?usp=sharing', 22: 'https://drive.google.com/file/d/1J9AVkTWm-lzJtq4Y9mOdVQAHcxjvDZaR/view?usp=sharing', 23: 'https://drive.google.com/file/d/1ZapJwNkO7MFGDH1PB3i6AZ7KCRBl48r2/view?usp=drivesdk'}
 import gdrivedl
 import os
 
+list = ['3', '4']
 title = './drumkit'
 url = 'https://drive.google.com/drive/folders/14PjpIRtPwvIInqngZoVJ1HTdMcHJfQNh?usp=sharing'
-command = 'py gdrivedl.py ' + url + ' -P ' + title
-# command = 'py gdrivedl.py ' + url
+
+# command = 'py gdrivedl.py ' + url + ' -P ' + title #windows
+command = 'python3 gdrivedl.py ' + url + ' -P ' + title #linux
 print(command)
 os.system(command)
 
 # os.system("py gdrivedl.py https://drive.google.com/drive/folders/1XVpJyWDIUHbmjbMHrU0OsNXeYy_RsupM?usp=sharing -P " + title)
 
 
+# if folder
+    # # command = 'py gdrivedl.py ' + url + ' -P ' + title #windows
+    # command = 'python3 gdrivedl.py ' + url + ' -P ' + title #linux
+    # print(command)
+    # os.system(command)
 
+# if file
+    # import gdown
+
+    # same as the above, and you can copy-and-paste a URL from Google Drive with fuzzy=True
+    # url = "https://drive.google.com/file/d/1HKfmfvm10RmaGUkJEvWJ6rgzr78ZbFOp/view?usp=sharing"
+    # output = "drumkit.zip"
+    # gdown.download(url=url, output=output, quiet=False, fuzzy=True)
+
+
+import zipfile
+with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+    zip_ref.extractall(directory_to_extract_to)
